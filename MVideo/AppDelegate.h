aa838d5c9e7b1ab15cf7bc9d953c858456cc899f
lihongli <honglili@sohu-inc.com>
2016-06-18 22:30:04 +0800

    Initial Commit

---
+++ MVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVideo
+//
+//  Created by LiHongli on 16/6/18.
+//  Copyright © 2016年 LHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVideo
+//
+//  Created by LiHongli on 16/6/18.
+//  Copyright © 2016年 LHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
